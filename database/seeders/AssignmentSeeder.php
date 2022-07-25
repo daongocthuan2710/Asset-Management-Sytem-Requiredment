6e9015df1b68ce247778c343e0253a46c211b03b
Vũ Hoàng Lâm <josephlam1304@gmail.com>
2022-07-25 17:02:48 +0700

    fix(be) change admin_id col in assignment table

--- database/seeders/AssignmentSeeder.php
+++ database/seeders/AssignmentSeeder.php
@@ -15,8 +15,8 @@ class AssignmentSeeder extends Seeder
      */
     public function run()
     {
-        Assignment::create(["asset_id" => "3", "admin_id" => 2, "staff_id" => 10, "assign_date" => "2022-02-20", "state" => 0]);
-        Assignment::create(["asset_id" => "5", "admin_id" => 7, "staff_id" => 12, "assign_date" => "2021-12-29", "state" => 1]);
-        Assignment::create(["asset_id" => "6", "admin_id" => 20, "staff_id" => 6, "assign_date" => "2020-10-10", "state" => -1]);
+        Assignment::create(["asset_id" => "3", "assigned_by" => 2, "staff_id" => 9, "assigned_date" => "2022-02-20", "state" => 0]);
+        Assignment::create(["asset_id" => "5", "assigned_by" => 7, "staff_id" => 24, "assigned_date" => "2021-12-29", "state" => 1]);
+        Assignment::create(["asset_id" => "6", "assigned_by" => 20, "staff_id" => 6, "assigned_date" => "2020-10-10", "state" => -1]);
     }
 }
