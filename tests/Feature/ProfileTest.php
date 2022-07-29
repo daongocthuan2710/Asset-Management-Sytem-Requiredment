89bed2d56a4eba022e344d1e1ded9c39036385d6
Nguyen Dang Hieu <danghieu140701@gmail.com>
2022-07-29 17:55:31 +0700

    feat(test): unit test profile

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -18,21 +18,36 @@ class ProfileTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function get_token()
+    public function get_token_user()
     {
         $response = $this->postJson('api/login', [
-            'username' => 'kienvv',
+            'username' => 'hieunm',
             'password' => '12345',
         ]);
         $response->assertStatus(200);
         $token = $response->getData()->token;
-        echo "toke".$token."  end";
+        return $token;
+    }
+
+    public function get_token_new_user()
+    {
+        $response = $this->postJson('api/login', [
+            'username' => 'tuannl',
+            'password' => '12345',
+        ]);
+        $response->assertStatus(200);
+        $token = $response->getData()->token;
         return $token;
     }
 
     public function test_profile()
     {
-        $token = $this->get_token();
+        $response = $this->postJson('api/login', [
+            'username' => 'trinm',
+            'password' => '12345',
+        ]);
+        $response->assertStatus(200);
+        $token = $response->getData()->token;
         $response = $this->getJson('/profile', [
             'Authorization' => "Bearer $token"
         ]);
@@ -41,7 +56,7 @@ class ProfileTest extends TestCase
 
     public function test_all_fields_has_not_been_entered_when_state_is_1()
     {
-        $token = $this->get_token();
+        $token = $this->get_token_user();
         $body = [
             "oldPassword" => "",
             "newPassword" => "12345",
@@ -56,7 +71,7 @@ class ProfileTest extends TestCase
 
     public function test_all_fields_has_been_entered_when_state_is_1()
     {
-        $token = $this->get_token();
+        $token = $this->get_token_user();
         $body = [
             "oldPassword" => "12345",
             "newPassword" => "12345"
@@ -71,7 +86,7 @@ class ProfileTest extends TestCase
 
     public function test_all_fields_has_not_been_entered_when_first_change_password()
     {
-        $token = $this->get_token();
+        $token = $this->get_token_new_user();
         $body = [
             "newPassword" => "",
         ];
@@ -83,23 +98,23 @@ class ProfileTest extends TestCase
             ->assertStatus(422);
     }
 
-    // public function test_all_fields_has_been_entered_when_first_change_password()
-    // {
-    //     $token = $this->get_token();
-    //     $body = [
-    //         "newPassword" => "12345"
-    //     ];
+    public function test_all_fields_has_been_entered_when_first_change_password()
+    {
+        $token = $this->get_token_new_user();
+        $body = [
+            "newPassword" => "12345"
+        ];
 
-    //     $this->json('POST', 'api/profile', $body, [
-    //         'Accept' => 'application/json',
-    //         'Authorization' => 'Bearer ' . $token
-    //     ])
-    //         ->assertStatus(200);
-    // }
+        $this->json('POST', 'api/profile', $body, [
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ])
+            ->assertStatus(200);
+    }
 
     public function test_old_password_is_incorrect()
     {
-        $token = $this->get_token();
+        $token = $this->get_token_user();
         $body = [
             "oldPassword" => "wrong-password",
             "newPassword" => "12345",
