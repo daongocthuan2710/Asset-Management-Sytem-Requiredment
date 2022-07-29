eb0cbcc624f7d19f026cfc8795cc72ec7d342ede
Huynh <nghuynh.dev@gmail.com>
2022-07-30 06:39:38 +0700

    Fix(fe) fix route

--- resources/js/Components/BodySection/index.js
+++ resources/js/Components/BodySection/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch, RefreshRoute } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Sidebar from "../Sidebar";
 import "./style.scss";
 import { ManageUser } from "../TableManageUser";
@@ -44,7 +44,6 @@ export default function BodySection() {
                     <Route path="/report">
                         <Test />
                     </Route>
-                    <RefreshRoute path='/' redirectionPath='/' />
                 </Switch>
             </div>
 
