52bcb8a3b93bc12f0adf637c5b72d4ece309a946
Nguyen Dang Hieu <danghieu140701@gmail.com>
2022-07-30 08:10:51 +0700

    format(be): styling code

--- app/Http/Controllers/Admin/ManageUserController.php
+++ app/Http/Controllers/Admin/ManageUserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Services\ManageUserService;
 use Illuminate\Http\Request;
-
 use App\Http\Requests\CreateUserRequest;
 
 class ManageUserController extends Controller
