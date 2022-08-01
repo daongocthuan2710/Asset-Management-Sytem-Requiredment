ba1c6e406520e6c2f01505488eff9a253b13352b
Nhon Nguyen <nhonnguyen3112@gmail.com>
2022-08-01 10:40:55 +0700

    fix(be): use resource and form request for api of edit and update

---
+++ app/Http/Requests/UpdateAssetRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\JoinedDateWeekend;
+use App\Rules\LatinName;
+use App\Rules\Over18;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class UpdateAssetRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['string', 'required'],
+            'specification' => ['string', 'required'],
+            'installed_date' => ['date', 'required'],
+            'state' => ['integer', 'required', 'max:1', 'min:-2'],
+        ];
+    }
+    public function messages()
+    {
+        return [];
+    }
+    protected function failedValidation(Validator|\Illuminate\Contracts\Validation\Validator $validator)
+    {
+
+        $errors = (new ValidationException($validator))->errors();
+        throw new HttpResponseException(response()->json(
+            [
+                "message" => "Validations fails",
+                'error' => $errors,
+            ],
+            400
+        ));
+    }
+}
--- app/Services/ManageAssetService.php
+++ app/Services/ManageAssetService.php
@@ -34,40 +34,22 @@ class ManageAssetService extends BaseService
 
     public function update($request, $id): \Illuminate\Http\JsonResponse
     {
-        //check admin
-        $sanctumUser = auth('sanctum')->user();
-        if (!$sanctumUser || !$sanctumUser->admin) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
-        //validate request
-        $validator = Validator::make($request->all(), [
-            'name' => 'string|required',
-            'specification' => 'string|required',
-            'installed_date' => 'date|required',
-            'state' => 'integer|required|max:1|min:-2',
-        ]);
-        if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], 400);
-        }
-        //check asset
-        $asset = DB::table('asset')->where('id', $id)->first();
-        if (!$asset) {
-            return response()->json(['error' => 'Asset not found'], 404);
-        }
-        //check admin location
-        if ($sanctumUser->location != $asset->location) {
-            return response()->json(['error' => 'You do not have permission to access this asset'], 401);
-        }
-        //check asset state
-        if ($asset->state == 2) {
-            return response()->json(['error' => 'Asset is assigned'], 422);
+        if ($this->checkPermission($request, $id) !== null) {
+            return $this->checkPermission($request, $id);
         }
         //update asset
         return $this->_manageAssetRepository->update($request, $id);
     }
     public function edit($request, $id): \Illuminate\Http\JsonResponse
     {
-        //check admin
+        if ($this->checkPermission($request, $id) !== null) {
+            return $this->checkPermission($request, $id);
+        }
+        //return asset
+        return $this->_manageAssetRepository->edit($request, $id);
+    }
+    public function checkPermission($request, $id): ?\Illuminate\Http\JsonResponse
+    {
         $sanctumUser = auth('sanctum')->user();
         if (!$sanctumUser || !$sanctumUser->admin) {
             return response()->json(['error' => 'Unauthorized'], 401);
@@ -82,10 +64,9 @@ class ManageAssetService extends BaseService
             return response()->json(['error' => 'You do not have permission to access this asset'], 401);
         }
         //check asset state
-        if ($asset->state == 2) {
+        if ($asset->state === 2) {
             return response()->json(['error' => 'Asset is assigned'], 422);
         }
-        //return asset
-        return $this->_manageAssetRepository->edit($request, $id);
+        return null;
     }
 }
