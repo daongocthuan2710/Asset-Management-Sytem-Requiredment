d766b20157938fafa6e7be706bd901e76b09166b
Nhon Nguyen <85271879+nhonnhon12@users.noreply.github.com>
2022-08-04 16:47:00 +0700

    feat(be): unit test for delete assignment

--- tests/Feature/DeleteAssignmentTest.php
+++ tests/Feature/DeleteAssignmentTest.php
@@ -2,9 +2,6 @@
 
 namespace Tests\Feature;
 
-use App\Models\Asset;
-use App\Models\Assignment;
-use App\Models\Category;
 use App\Models\User;
 use Database\Seeders\AssetSeeder;
 use Database\Seeders\AssignmentSeeder;
@@ -15,7 +12,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
-class GetDeleteAssignmentTest extends TestCase
+class DeleteAssignmentTest extends TestCase
 {
     public function setUp(): void
     {
@@ -27,33 +24,38 @@ class GetDeleteAssignmentTest extends TestCase
             AssignmentSeeder::class,
         ]);
     }
-    public function test_not_logged_user(){
-        $id = 10; //staff_location: DN, state: 0
-        $this->json('GET', "api/can-delete-assignment/$id")
+    public function test_not_logged_user()
+    {
+        $id = 10; //location: DN, state: 0
+        $this->json('DELETE', "api/assignment/$id")
             ->assertStatus(401);
     }
-    public function test_not_an_admin(){
+    public function test_not_an_admin_user()
+    {
         $id = 10; //staff_location: DN, state: 0
         Sanctum::actingAs(User::findOrFail(39)); //admin: false, location: DN, state: 1
-        $this->json('GET', "api/can-delete-assignment/$id")
+        $this->json('DELETE', "api/assignment/$id")
             ->assertStatus(401);
     }
-    public function test_not_same_location(){
+    public function test_not_same_location()
+    {
         $id = 2; //staff_location: HCM, state: 0
         Sanctum::actingAs(User::findOrFail(37)); //admin, location: DN, state: 1
-        $this->json('GET', "api/can-delete-assignment/$id")
+        $this->json('DELETE', "api/assignment/$id")
             ->assertStatus(401);
     }
-    public function test_delete_accepted_assignment(){
+    public function test_delete_accepted_assignment()
+    {
         $id = 9; //staff_location: DN, state: 1
         Sanctum::actingAs(User::findOrFail(37)); //admin, location: DN, state: 1
-        $this->json('GET', "api/can-delete-assignment/$id")
+        $this->json('DELETE', "api/assignment/$id")
             ->assertStatus(422);
     }
-    public function test_success_get_can_delete_assignment(){
-        $id = 10; //staff_location: DN, state: 0
+    public function test_success_delete_assignment()
+    {
+        $id = 10; //location: DN, state: 0
         Sanctum::actingAs(User::findOrFail(37)); //admin, location: DN, state: 1
-        $this->json('GET', "api/can-delete-assignment/$id")
+        $this->json('DELETE', "api/assignment/$id")
             ->assertStatus(200);
     }
 }
