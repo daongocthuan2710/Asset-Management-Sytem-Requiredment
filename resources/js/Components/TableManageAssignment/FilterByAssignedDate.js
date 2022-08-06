7e45f877fa37346ee5dc6b55dfde47e969a7f489
Nguyen Dang Hieu <danghieu140701@gmail.com>
2022-08-06 18:45:08 +0700

    fix(fe+be): asset and assignment date picker

--- resources/js/Components/TableManageAssignment/FilterByAssignedDate.js
+++ resources/js/Components/TableManageAssignment/FilterByAssignedDate.js
@@ -6,44 +6,44 @@ import React, { useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import moment from "moment";
 
-import {
-    Dropdown, Form
-} from "react-bootstrap";
-import {
-    FaFilter
-} from 'react-icons/fa';
+import { Dropdown, Form } from "react-bootstrap";
+import { FaFilter } from "react-icons/fa";
 export default function FilterByAssignedDate({
-    currentButton,
-    handleFilter,
-    arrayState,
-    handleFilterDate
-}) 
+  currentButton,
+  handleFilter,
+  arrayState,
+  handleFilterDate,
+}) {
+  const [startDate, setStartDate] = useState(new Date());
+  const [datePickerOpen, setDatePickerOpen] = useState(false);
 
-{
-    const [startDate, setStartDate] = useState(new Date());
-        
-    return (    
-        <>
-            <Dropdown >
-                <Dropdown.Toggle id="button-assigned-date" className="filter-button d-flex align-items-center justity-content-center ">
-                    <p className="flex-grow-1 font-weight-bold mb-0">Assigned Date</p>
-                    <div className="fb-icon">
-                        <FaFilter />
-                    </div>
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                <Form.Group controlId="formBasicDate">
-
-
-            
-                <DatePicker  dateFormat="dd/MM/yyyy" selected={startDate}  onChange={(startDate) =>{ 
-                    handleFilterDate(moment(startDate).format('DD-MM-YYYY'));
-                    setStartDate(startDate);
-                }}  />
-         
-          </Form.Group>
-                </Dropdown.Menu>
-            </Dropdown>
-        </>
-    );
+  return (
+    <>
+      <Dropdown onClick={() => setDatePickerOpen(true)}>
+        <Dropdown.Toggle
+          id="button-assigned-date"
+          className="filter-button d-flex align-items-center justity-content-center "
+        >
+          <p className="flex-grow-1 font-weight-bold mb-0">Assigned Date</p>
+          <div className="fb-icon">
+            <FaFilter />
+          </div>
+        </Dropdown.Toggle>
+        <Dropdown.Menu id="menu-datepicker">
+          <DatePicker
+            open={datePickerOpen}
+            onOpenChange={setDatePickerOpen}
+            className="datePicker"
+            maxDate={new Date()}
+            dateFormat="MM/dd/yyyy"
+            selected={startDate}
+            onChange={(startDate) => {
+              handleFilterDate(moment(startDate).format("YYYY-MM-DD"));
+              setStartDate(startDate);
+            }}
+          />
+        </Dropdown.Menu>
+      </Dropdown>
+    </>
+  );
 }
